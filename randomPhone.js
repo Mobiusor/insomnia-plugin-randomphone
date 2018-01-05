29712c6474e38edada59ee69638e2754a1ced951
Mobiusor <mobiusor@gmail.com>
2018-01-05 16:49:05 +0800

    edit body

--- randomPhone.js
+++ randomPhone.js
@@ -6,15 +6,7 @@ module.exports.templateTags = [{
     async run (context) {
         min = 0
         max = 10000000
-        if (max.toString().length > length && length !== 0) {
-            throw new Error("Length has to be bigger than the max number's length");
-        }
-
-        if (min > max) {
-            throw new Error("Max has to be bigger than min");
-        }
         prefix = 152;
-
         randomNumber = (Math.floor(Math.random() * (max - min)) + min);
         return (randomNumber + prefix * 100000000).toString();
     }
